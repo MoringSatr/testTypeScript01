84d92f7227dabdf126f7e709a04a88c4ac583a49
liubowen <liubowen_94@163.com>
2018-01-25 01:43:51 +0800

    sad

--- assets/scripts/Constant.js
+++
@@ -1,9 +0,0 @@
-"use strict";
-Object.defineProperty(exports, "__esModule", { value: true });
-var Constant = /** @class */ (function () {
-    function Constant() {
-    }
-    Constant.SOCKET_URL = "http://127.0.0.1:9500";
-    return Constant;
-}());
-exports.default = Constant;
