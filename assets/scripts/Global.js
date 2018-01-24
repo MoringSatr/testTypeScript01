8fda52738ebab897bb6774844a87907d2bb50027
liubowen <liubowen_94@163.com>
2018-01-25 01:44:55 +0800

    sad

---
+++ assets/scripts/Global.js
@@ -0,0 +1,19 @@
+"use strict";
+Object.defineProperty(exports, "__esModule", { value: true });
+var Panel_1 = require("./Panel");
+var SocketUtil_1 = require("./SocketUtil");
+var Global = /** @class */ (function () {
+    function Global() {
+    }
+    Global.getInstance = function () {
+        return this.instance;
+    };
+    Global.prototype.load = function () {
+        this.Panel = new Panel_1.default();
+        this.Panel.load();
+        this.SocketUtil = new SocketUtil_1.default();
+    };
+    Global.instance = new Global();
+    return Global;
+}());
+exports.default = Global;
